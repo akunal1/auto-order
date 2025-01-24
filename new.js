1c3ee423c5ed2e13ed88886ffb691b98ba73f01f
akunal1 <mail.kunal71@gmail.com>
2025-01-24 18:33:01 +0530

    Update new.js

--- new.js
+++ new.js
@@ -49,7 +49,7 @@ const saveLastIndex = (index) => {
     }
 
     const browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 
