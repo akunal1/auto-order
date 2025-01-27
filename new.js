ee2fae530ef222cdb18f973ff913efebf3dd2914
akunal1 <mail.kunal71@gmail.com>
2025-01-27 12:02:13 +0530

    Update new.js

--- new.js
+++ new.js
@@ -11,7 +11,9 @@ function generatePhoneNumber() {
   const startingDigit = [9, 8, 7][Math.floor(Math.random() * 3)];
 
   // Generate the remaining 9 digits randomly
-  const remainingDigits = Array.from({ length: 9 }, () => Math.floor(Math.random() * 10)).join('');
+  const remainingDigits = Array.from({ length: 9 }, () =>
+    Math.floor(Math.random() * 10)
+  ).join("");
 
   // Combine starting digit with the remaining digits
   const phoneNumber = `${startingDigit}${remainingDigits}`;
@@ -69,10 +71,15 @@ const saveLastIndex = (index) => {
       const page = await browser.newPage();
 
       const urls = [
-        "https://seedghani.com/products/first-rain-perfume-oil-699-12ml-inspire",
-        "https://seedghani.com/products/first-rain-attar-799-12ml",
+        "https://kannaujattarr.com/products/first-rain-attar-12ml",
+        "https://kannaujattarr.com/products/gulab-natural-attar-12ml",
+        "https://kannaujattarr.com/products/shamama-attar-handcrafted-fragrance-from-kannauj",
       ];
-      const randomUrl = urls[Math.floor(Math.random() * urls.length)];
+      // Generate a random index between 0 and the last index of the array
+      const randomIndex = Math.floor(Math.random() * urls.length);
+
+      // Pick a random URL from the array
+      const randomUrl = urls[randomIndex];
       await page.goto(randomUrl, { waitUntil: "networkidle2", timeout: 60000 });
       console.log(`Navigated to ${randomUrl}`);
 
@@ -94,76 +101,50 @@ const saveLastIndex = (index) => {
 
       await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait 5 seconds
 
-      // Refresh the page
-      await page.reload({ waitUntil: "networkidle2" });
-      console.log("Page refreshed.");
+      // // Refresh the page
+      // await page.reload({ waitUntil: "networkidle2" });
+      // console.log("Page refreshed.");
 
-      //   await new Promise((resolve) => setTimeout(resolve, 10000)); // Wait 10 seconds
+      // await page.goto("https://kannaujfragrances.in/cart", {
+      //   waitUntil: "networkidle2",
+      //   timeout: 60000,
+      // });
 
-      // Click on the cart icon
-      //   const cartIconId = "#cart-icon-bubble";
-      //   await page.waitForSelector(cartIconId, { timeout: 20000 });
-      //   await page
-      //     .click(cartIconId)
-      //     .catch(() => console.log("Cart icon not found"));
+      // console.log("Navigated to cart page");
 
-      await page.goto("https://seedghani.com/cart", {
-        waitUntil: "networkidle2",
-        timeout: 60000,
-      });
+      // // Click on the checkout button
+      // const checkoutButtonId = ".es-popup-button-cart"; //".cart__checkout-button";
 
-      console.log("Navigated to cart page");
-      //   await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait 5 seconds
+      // await page.waitForSelector(checkoutButtonId, { timeout: 10000 });
 
-      // Click on the checkout button
-      const checkoutButtonId = "#checkout"; //".cart__checkout-button";
-      //   const checkoutButtonExists = await page.$(checkoutButtonId);
+      // await page.click(checkoutButtonId);
+      // console.log("checkoutButtonId button clicked.");
+      // await new Promise((resolve) => setTimeout(resolve, 15000)); // Wait 15 seconds
+      // console.log("waited 5 seconds");
 
-      await page.waitForSelector(checkoutButtonId, { timeout: 10000 });
-
-      await page.click(checkoutButtonId);
-      console.log("checkoutButtonId button clicked.");
-      await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait 15 seconds
-      console.log("waited 5 seconds");
-
-      //   const popup_button = "#checkout";
-      //   await page.waitForSelector(popup_button, { timeout: 10000 });
-      //   await page.click(popup_button);
-
-      //   await new Promise((resolve) => setTimeout(resolve, 5000));
-
-      // Fill the form fields
-      //   const i = Math.floor(Math.random() * formFields.length);
       const data = formFields[i];
 
-      const typeLikeHuman = async (selector, text) => {
-        for (const char of text) {
-          await page.type(selector, char, { delay: Math.random() * 100 + 50 });
-        }
-      };
-
-      await page.waitForSelector('input[name="email"]', {
+      await page.waitForSelector('input[name="first_name"]', {
         visible: true,
-        timeout: 10000,
+        timeout: 30000,
       });
 
-      let contact = data.email 
-      if((data.email??"").length===10){
-       
-          contact =  generatePhoneNumber()
-        
-      }
+      let contact = generatePhoneNumber();
 
-      await page.type('input[name="email"]', contact, {
+      await page.type('input[name="phone"]', contact, {
         delay: Math.random() * 200 + 50,
       });
-      await page.type('input[name="firstName"]', data.firstName, {
-        delay: Math.random() * 200 + 50,
-      });
-      await page.type('input[name="lastName"]', data.lastName, {
-        delay: Math.random() * 200 + 50,
-      });
-      await page.type('input[name="address1"]', data.address1, {
+      await page.type(
+        'input[name="first_name"]',
+        data.firstName + " " + data.lastName,
+        {
+          delay: Math.random() * 200 + 50,
+        }
+      );
+      // await page.type('input[name="lastName"]', data.lastName, {
+      //   delay: Math.random() * 200 + 50,
+      // });
+      await page.type('input[name="address"]', data.address1, {
         delay: Math.random() * 200 + 50,
       });
       await page.type('input[name="address2"]', data.address2, {
@@ -172,24 +153,27 @@ const saveLastIndex = (index) => {
       await page.type('input[name="city"]', data.city, {
         delay: Math.random() * 200 + 50,
       });
-      await page.select('select[name="zone"]', data.zone);
-      await page.type('input[name="postalCode"]', data.postalCode.toString(), {
+      // await page.select('select[name="province"]', data.zone);
+
+      await page.evaluate((state) => {
+        const selectElement = document.querySelector('select[name="province"]');
+        const options = Array.from(selectElement.options);
+
+        // Find the option with the matching text
+        const matchingOption = options.find(
+          (option) => option.text.trim() === state
+        );
+        if (matchingOption) {
+          matchingOption.selected = true; // Set it as selected
+          selectElement.dispatchEvent(new Event("change", { bubbles: true })); // Trigger change event
+        } else {
+          console.error(`State "${state}" not found in dropdown.`);
+        }
+      }, data.city);
+
+      await page.type('input[name="zip"]', data.postalCode.toString(), {
         delay: Math.random() * 200 + 50,
       });
-
-      //   await typeLikeHuman('input[name="email"]', data.email);
-      //   await typeLikeHuman('input[name="firstName"]', data.firstName);
-      //   await typeLikeHuman('input[name="lastName"]', data.lastName);
-      //   await typeLikeHuman('input[name="address1"]', data.address1);
-      //   if (data.address2) {
-      //     await typeLikeHuman('input[name="address2"]', data.address2);
-      //   }
-      //   await typeLikeHuman('input[name="city"]', data.city);
-      //   await page.select('select[name="zone"]', data.zone);
-      //   await typeLikeHuman(
-      //     'input[name="postalCode"]',
-      //     data.postalCode.toString()
-      //   );
 
       console.log("Form filled with data:", data);
       console.log("contact info:", contact);
@@ -197,24 +181,32 @@ const saveLastIndex = (index) => {
       await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait 10 seconds
 
       // Click on the pay button
-      const payButtonId = "#checkout-pay-button";
+      const payButtonId = "#es-form-button";
       await page
         .click(payButtonId)
         .catch(() => console.log("Pay button not found"));
 
       await new Promise((resolve) => setTimeout(resolve, 20000)); // Wait 30 seconds
+      const currentUrl = page.url(); // Get the current URL
+      if (currentUrl.includes("https://shopify.com/")) {
+        console.log("<<<<<<<<<<<<<< success >>>>>>>>>>>>>>");
+      } else {
+        console.log("URL does not contain 'https://shopify.com/'. Breaking...");
+        break;
+      }
     } catch (error) {
       console.error("Error during automation:", error);
     } finally {
       i++;
       j++;
       saveLastIndex(i); // Save the current value of `i`
+      //https://shopify.com/
       await browser.close();
       console.log("Browser closed. Restarting process...");
-      if(j>35){
-          break;
+
+      if (j > 40) {
+        break;
       }
-    
     }
   }
 })();
